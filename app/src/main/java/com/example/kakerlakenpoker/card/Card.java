14e5ca5d989f95fefa818351e8a66fc0ec713d07
Lukas <lukasbu@edu.aau.at>
2021-04-25 13:51:25 +0200

    Card Klasse mit getter und setter erstellt Benötigt ENUM Type als Parameter im Konstruktor

--- app/src/main/java/com/example/kakerlakenpoker/card/Card.java
+++ app/src/main/java/com/example/kakerlakenpoker/card/Card.java
@@ -1,4 +1,17 @@
 package com.example.kakerlakenpoker.card;
 
 public class Card {
+    private Type type;
+
+    public Card(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 }
