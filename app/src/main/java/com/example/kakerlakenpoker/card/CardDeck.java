697020dd0d505d39c2aa80aef0a6b291058acf01
Lukas <lukasbu@edu.aau.at>
2021-04-26 08:44:32 +0200

    CardDeck Klasse erstellt Repräsentiert eine Liste mit allen 64 Spielkarten

---
+++ app/src/main/java/com/example/kakerlakenpoker/card/CardDeck.java
@@ -0,0 +1,18 @@
+package com.example.kakerlakenpoker.card;
+
+import java.util.ArrayList;
+
+/**
+ * Deck Klasse die ein Deck mit allen 64 Karten repräsentiert.
+ */
+public class CardDeck {
+    private final ArrayList<Card> deck = new ArrayList<>();
+
+    public CardDeck() {
+      createDeck();
+    }
+
+    private void createDeck() {
+
+    }
+}
