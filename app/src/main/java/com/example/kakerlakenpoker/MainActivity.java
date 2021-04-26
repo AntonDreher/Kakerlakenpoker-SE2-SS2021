08d7819632715dae033f8edd060ea7288b252f8d
Johann Wriesnig <jowriesnig@edu.aau.at>
2021-04-26 21:11:52 +0200

    [Lobby View] Views für die Lobbysuche hinzugefügt

--- app/src/main/java/com/example/kakerlakenpoker/MainActivity.java
+++ app/src/main/java/com/example/kakerlakenpoker/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Intent multi = new Intent(this, MultiplayerMainActivity.class);
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/kakerlakenpoker/SearchLobbyActivity.java
@@ -0,0 +1,38 @@
+package com.example.kakerlakenpoker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class SearchLobbyActivity extends AppCompatActivity {
+    Intent intent;
+    FloatingActionButton floatingActionButton;
+    Button join;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchlobbyview);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener((View view)->goBack());
+
+        join = findViewById(R.id.joinButton);
+        join.setOnClickListener((View view)->joinUp());
+
+
+    }
+
+    public void goBack(){
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void joinUp(){}
+
+
+}
