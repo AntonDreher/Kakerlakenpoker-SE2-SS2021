2a7aad865fa6b1bed75ae5d57797fa18cb319981
Lukas <lukasbu@edu.aau.at>
2021-04-26 09:07:07 +0200

    Methode zum erstellen des Decks erstellt

--- app/src/main/java/com/example/kakerlakenpoker/card/CardDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/card/CardDeck.java
@@ -6,13 +6,21 @@ import java.util.ArrayList;
  * Deck Klasse die ein Deck mit allen 64 Karten repräsentiert.
  */
 public class CardDeck {
-    private final ArrayList<Card> deck = new ArrayList<>();
+    private final ArrayList<Card> deck = new ArrayList<>(64);
 
     public CardDeck() {
       createDeck();
     }
 
+    /**
+     * Methode die das Deck mit 64 Karten erstellt
+     */
     private void createDeck() {
-
+        for (Type t : Type.values()) {
+            for (int i = 0; i < 8; i++) {
+                deck.add(new Card(t));
+            }
+        }
     }
 }
+
