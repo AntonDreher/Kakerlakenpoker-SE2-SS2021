e6739eee77200984eaef5e7d32c0f5b278c313dd
Nik <nitschernitz@edu.at>
2021-04-26 18:37:16 +0200

    Ausbesserung der StartView! Eigene Landscape View erstellt. Button wurde implementiert (+ neue Klasse für MultiplayerView) App startet in Landscape Modus.

--- app/src/main/java/com/example/kakerlakenpoker/MainActivity.java
+++ app/src/main/java/com/example/kakerlakenpoker/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.kakerlakenpoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    Intent multi = new Intent(this, MultiplayerMainActivity.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.startview);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNextView(multi);
+            }
+        });
+
+    }
+
+    public void getNextView (Intent intent){
+        startActivity(intent);
     }
 }
 
---
+++ app/src/main/java/com/example/kakerlakenpoker/MultiplayerMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kakerlakenpoker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MultiplayerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
