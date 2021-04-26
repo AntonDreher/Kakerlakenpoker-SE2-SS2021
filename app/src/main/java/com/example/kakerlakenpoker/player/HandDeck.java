ae0cdafd29946648d857a81c2593d71b77603d72
Lukas <lukasbu@edu.aau.at>
2021-04-26 15:38:27 +0200

    add Card Methode in Klasse HandDeck erstellt

--- app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
@@ -1,7 +1,19 @@
 package com.example.kakerlakenpoker.player;
+import com.example.kakerlakenpoker.card.Card;
+import java.util.ArrayList;
 
 /**
  * Repräsentiert die Handkarten eines Spielers
  */
+
 public class HandDeck {
+    private final ArrayList<Card> deck = new ArrayList<>();
+
+    /**
+     * Fügt dem Handdeck eine Karte hinzu
+     * @param card Kommt aus dem CardDeck oder von einem anderen Spieler
+     */
+    public void addCard(Card card){
+        deck.add(card);
+    }
 }
