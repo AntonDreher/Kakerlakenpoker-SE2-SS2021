59a14484f66d0fa4d8c0787870f79a8d1c03a6fd
Lukas <lukasbu@edu.aau.at>
2021-04-27 09:12:19 +0200

    Player Klasse Attribute, Getter + Setter + Konstruktor erstellt

--- app/src/main/java/com/example/kakerlakenpoker/player/Player.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/Player.java
@@ -5,4 +5,27 @@ package com.example.kakerlakenpoker.player;
  * Diese Klasse verwendet die Klasse HandDeck
  */
 public class Player {
+    private String name;
+    private HandDeck handDeck;
+
+    public Player(String name, HandDeck handDeck) {
+        this.name = name;
+        this.handDeck = handDeck;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HandDeck getHandDeck() {
+        return handDeck;
+    }
+
+    public void setHandDeck(HandDeck handDeck) {
+        this.handDeck = handDeck;
+    }
 }
