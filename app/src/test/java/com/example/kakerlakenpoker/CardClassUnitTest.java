e63d9b6d81c2bf71e9e941bde0d7e6d597284091
Lukas <lukasbu@edu.aau.at>
2021-05-06 14:18:12 +0200

    Unit Tests für Klasse Card erstellt (CardClassUnitTest)

---
+++ app/src/test/java/com/example/kakerlakenpoker/CardClassUnitTest.java
@@ -0,0 +1,32 @@
+package com.example.kakerlakenpoker;
+
+import com.example.kakerlakenpoker.card.Card;
+import com.example.kakerlakenpoker.card.Type;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+public class CardClassUnitTest {
+    private Card one;
+
+    @BeforeEach
+    public void setUp(){
+        this.one = new Card(Type.KRÖTE);
+    }
+    @AfterEach
+    public void tearDown(){
+        this.one = null;
+    }
+
+    @Test
+    public void testGetType(){
+        Assertions.assertEquals(Type.KRÖTE,this.one.getType());
+    }
+
+    @Test
+    public void testSetType(){
+        this.one.setType(Type.FLEDERMAUS);
+        Assertions.assertEquals(Type.FLEDERMAUS,this.one.getType());
+    }
+}
