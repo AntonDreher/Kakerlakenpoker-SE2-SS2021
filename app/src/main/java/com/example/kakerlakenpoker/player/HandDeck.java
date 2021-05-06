4c64684806b4aa887b57f3584415681516bd5d61
Lukas <lukasbu@edu.aau.at>
2021-05-06 13:05:21 +0200

    Klasse HandDeck im Package Card überarbeitet. Ist jetzt leer, wird aber sicher noch mit Methoden gefüllt

--- app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
@@ -1,5 +1,6 @@
 package com.example.kakerlakenpoker.player;
 import com.example.kakerlakenpoker.card.Card;
+import com.example.kakerlakenpoker.card.CardDeck;
 import com.example.kakerlakenpoker.card.Type;
 
 import java.util.ArrayList;
@@ -9,55 +10,10 @@ import java.util.List;
  * Repräsentiert die Handkarten eines Spielers
  */
 
-public class HandDeck {
-    private final ArrayList<Card> deck = new ArrayList<>();
+public class HandDeck extends CardDeck {
 
-    /**
-     * Fügt dem Handdeck eine Karte hinzu
-     * @param card Kommt aus dem CardDeck oder von einem anderen Spieler
-     */
-    public void addCard(Card card){
-        deck.add(card);
-    }
 
-    /**
-     * Entfernt eine Karte aus dem HandDeck
-     * @param type Handdeck wird nach ENUM durchsucht
-     */
-    public void removeCard(Type type){
-        Card c = findCard(type);
-        if(c != null){
-            deck.remove(c);
-        }
-    }
+      //Platz für Methoden des HandDecks, wird erst später implementiert.
 
-    /**
-     * Durchsucht das Deck nach einer Karte des gesuchten Types
-     * @param type eines der 8 ENUMS
-     * @return Karte, wenn gefunden. Ansonsten null
-     */
-    public Card findCard(Type type){
-        for(Card c: deck){
-            if(c.getType().equals(type)){
-                return c;
-            }
-        }
-        return null;
-    }
 
-    /**
-     * Methode, die die Anzahl der HandKarten zurück gibt
-     * @return int, Anzahl der Handkarten
-     */
-    public int size(){
-        return deck.size();
-    }
-
-    /**
-     * Getter Methode
-     * @return akutelle Handkarten
-     */
-    public List <Card> getDeck() {
-        return deck;
-    }
 }
