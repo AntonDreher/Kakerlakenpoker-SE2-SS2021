285981a5b4dd3c3f12f9fdaf7a30fc40a60f43b5
Lukas <lukasbu@edu.aau.at>
2021-05-06 13:00:27 +0200

    Klasse GameDeck im Package Card überarbeitet.

--- app/src/main/java/com/example/kakerlakenpoker/card/GameDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/card/GameDeck.java
@@ -5,15 +5,10 @@ import java.util.ArrayList;
 /**
  * Deck Klasse die ein Deck mit allen 64 Karten repräsentiert.
  */
-public class GameDeck {
-    private final ArrayList<Card> deck = new ArrayList<>(64);
-
+public class GameDeck extends CardDeck {
+    boolean createdDeck = false;
     public GameDeck() {
-      createDeck();
-    }
-
-    public ArrayList<Card> getDeck() {
-        return deck;
+        createDeck();
     }
 
     /**
@@ -22,8 +17,23 @@ public class GameDeck {
     private void createDeck() {
         for (Type t : Type.values()) {
             for (int i = 0; i < 8; i++) {
-                deck.add(new Card(t));
+                addCard(new Card(t));
             }
+        }
+        createdDeck = true;
+    }
+
+    /**
+     * Checkt in der IF, ob GameDeck erstellt wurde, wenn Ja gibts eine Exception
+     * @param card ENUM aus Klasse Card
+     */
+    @Override
+    public void addCard(Card card) {
+        if(createdDeck){
+            throw new IllegalArgumentException("Cannot add");
+        }
+        else{
+            super.addCard(card);
         }
     }
 }
