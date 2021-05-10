6b8b097d4b3aa28855485b63a9455f29cfabd479
EvaH1 <evahe@edu.aau.at>
2021-05-10 19:39:06 +0200

    lostGame method created in CollectedDeck class

--- app/src/main/java/com/example/kakerlakenpoker/player/CollectedDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/CollectedDeck.java
@@ -1,5 +1,6 @@
 package com.example.kakerlakenpoker.player;
 
+import com.example.kakerlakenpoker.card.Card;
 import com.example.kakerlakenpoker.card.GameDeck;
 
 /**
@@ -8,5 +9,40 @@ import com.example.kakerlakenpoker.card.GameDeck;
 public class CollectedDeck extends GameDeck {
 
     //Platz für Methoden
+    public boolean lostGame() {
+        int fledermaus = 0, fliege = 0, ratte = 0, scorpion = 0, kakerlake = 0,  kroete = 0, spinne = 0, stinkwanze = 0;
 
+        for (Card c : super.getDeck()) {
+            switch (c.getType().toString()) {
+                case "FLEDERMAUS":
+                    fledermaus++;
+                    break;
+                case "FLIEGE":
+                    fliege++;
+                    break;
+                case "RATTE":
+                    ratte++;
+                    break;
+                case "SCORPION":
+                    scorpion++;
+                    break;
+                case "KAKERLAKE":
+                    kakerlake++;
+                    break;
+                case "KROETE":
+                    kroete++;
+                    break;
+                case "SPINNE":
+                    spinne++;
+                    break;
+                case "STINKWANZE":
+                    stinkwanze++;
+                    break;
+            }
+        }
+
+        if (fledermaus == 4 || fliege == 4 || ratte == 4 || scorpion == 4 || kakerlake == 4 || kroete == 4 || spinne == 4 || stinkwanze == 4 ) {
+            return true;
+        } else return fledermaus == 1 && fliege == 1 && ratte == 1 && scorpion == 1 && kakerlake == 1 && kroete == 1 && spinne == 1 && stinkwanze == 1;
+    }
 }
