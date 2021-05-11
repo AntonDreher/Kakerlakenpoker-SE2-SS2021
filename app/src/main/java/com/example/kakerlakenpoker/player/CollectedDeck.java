6684be9f1d1676260c060f6beaed9ae83ad24044
EvaH1 <evahe@edu.aau.at>
2021-05-11 09:23:02 +0200

    duplicated code in CollectedDeck and HandDeck class refactored; countAllCards method in CardDeck class created

--- app/src/main/java/com/example/kakerlakenpoker/player/CollectedDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/CollectedDeck.java
@@ -13,48 +13,11 @@ public class CollectedDeck extends CardDeck {
      */
 
     public boolean lostGame() {
-        int fledermaus = 0;
-        int fliege = 0;
-        int ratte = 0;
-        int scorpion = 0;
-        int kakerlake = 0;
-        int kroete = 0;
-        int spinne = 0;
-        int stinkwanze = 0;
+        super.countAllCards();
 
-        for (Card c : super.getDeck()) {
-            switch (c.getType().toString()) {
-                case "FLEDERMAUS":
-                    fledermaus++;
-                    break;
-                case "FLIEGE":
-                    fliege++;
-                    break;
-                case "RATTE":
-                    ratte++;
-                    break;
-                case "SCORPION":
-                    scorpion++;
-                    break;
-                case "KAKERLAKE":
-                    kakerlake++;
-                    break;
-                case "KROETE":
-                    kroete++;
-                    break;
-                case "SPINNE":
-                    spinne++;
-                    break;
-                case "STINKWANZE":
-                    stinkwanze++;
-                    break;
-                default: break;
-            }
-        }
-
-        if (fledermaus == 4 || fliege == 4 || ratte == 4 || scorpion == 4 || kakerlake == 4 || kroete == 4 || spinne == 4 || stinkwanze == 4) {
+        if (this.getFledermaus() == 4 || this.getFliege() == 4 || this.getRatte() == 4 || this.getScorpion() == 4 || this.getKakerlake()== 4 || this.getKroete() == 4 || this.getSpinne() == 4 || this.getStinkwanze() == 4) {
             return true;
         } else
-            return fledermaus == 1 && fliege == 1 && ratte == 1 && scorpion == 1 && kakerlake == 1 && kroete == 1 && spinne == 1 && stinkwanze == 1;
+            return this.getFledermaus() ==  1 &&  this.getFliege() ==  1 &&  this.getRatte() ==  1 &&  this.getScorpion() ==  1 && this.getKakerlake()==  1 &&  this.getKroete() ==  1 && this.getSpinne() == 1 && this.getStinkwanze() == 1;
     }
 }
--- app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
+++ app/src/main/java/com/example/kakerlakenpoker/player/HandDeck.java
@@ -8,35 +8,10 @@ import com.example.kakerlakenpoker.card.CardDeck;
 
 public class HandDeck extends CardDeck {
 
-    int fledermaus = 0;
-    int fliege = 0;
-    int ratte = 0;
-    int scorpion = 0;
-    int kakerlake = 0;
-    int kroete = 0;
-    int spinne = 0;
-    int stinkwanze = 0;
-
     public void displayCard(){
         // Updatet die TextView in GameView
-
-        for(int i = 0; i<getDeck().size(); i++) {
-
-            switch (getDeck().get(i).getType().toString()) {
-
-                case "FLEDERMAUS": fledermaus++; break;
-                case "FLIEGE": fliege++; break;
-                case "RATTE": ratte++; break;
-                case "SCORPION": scorpion++; break;
-                case "KAKERLAKE": kakerlake++; break;
-                case "KROETE": kroete++; break;
-                case "SPINNE": spinne++; break;
-                case "STINKWANZE": stinkwanze++; break;
-                default:break;
-            }
+        super.countAllCards();
             // Set TextView mit den gezählten werten.
-
-        }
     }
 
     //Die Methode wird aufgerufen, wenn ein Spieler eine Karte ausspielt.
@@ -44,38 +19,6 @@ public class HandDeck extends CardDeck {
         removeCard(card.getType());
 
         //Hier muss dann die ausgespielte Karte weitergegebn werden (Server-Client)
-    }
-
-    public int getFledermaus() {
-        return fledermaus;
-    }
-
-    public int getFliege() {
-        return fliege;
-    }
-
-    public int getRatte() {
-        return ratte;
-    }
-
-    public int getScorpion() {
-        return scorpion;
-    }
-
-    public int getKakerlake() {
-        return kakerlake;
-    }
-
-    public int getKroete() {
-        return kroete;
-    }
-
-    public int getSpinne() {
-        return spinne;
-    }
-
-    public int getStinkwanze() {
-        return stinkwanze;
     }
 
 }
