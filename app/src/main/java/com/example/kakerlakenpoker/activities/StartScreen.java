dc2eee67da8a77211be0667994ce7e9199af6310
Johann Wriesnig <jowriesnig@edu.aau.at>
2021-06-10 20:34:13 +0200

    added startscreen

---
+++ app/src/main/java/com/example/kakerlakenpoker/activities/StartScreen.java
@@ -0,0 +1,28 @@
+package com.example.kakerlakenpoker.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.esotericsoftware.minlog.Log;
+import com.example.kakerlakenpoker.R;
+
+public class StartScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreenview);
+
+        Intent intent = new Intent(this, EnterUserNameActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        },4000);
+    }
+}
