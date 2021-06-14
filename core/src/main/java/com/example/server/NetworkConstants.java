1df1b7b0edaa127612010091a2a145238c83759a
Johann Wriesnig <jowriesnig@edu.aau.at>
2021-06-14 23:06:52 +0200

    changed ip

--- core/src/main/java/com/example/server/NetworkConstants.java
+++ core/src/main/java/com/example/server/NetworkConstants.java
@@ -2,5 +2,5 @@ package com.example.server;
 
 public class NetworkConstants {
     public static final int TCP_PORT = 53211;
-    public static final String MAIN_SERVER_IP = "192.168.56.1"; //se2-demo.aau.at
+    public static final String MAIN_SERVER_IP = "se2-demo.aau.at"; //se2-demo.aau.at
 }
